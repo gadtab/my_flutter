c430f70b6a3f5e1784eb0d3821af28362339cd0c
Gadi Tabachnik <gadtab@gmail.com>
2020-11-02 22:33:08 +0200

    world time app part 5

--- myapp/lib/pages/choose_location.dart
+++ myapp/lib/pages/choose_location.dart
@@ -6,26 +6,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'gadi';
-    });
-
-    // simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'father, husband, programmer';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print ('hey there');
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +17,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-
     );
   }
 }
