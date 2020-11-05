4716eeb2708fd4be64c5bbbdf3e1f109d481ba8c
Gadi Tabachnik <gadtab@gmail.com>
2020-11-05 21:10:42 +0200

    Get Register to work

--- flutter-with-firebase/brew_crew/lib/services/auth.dart
+++ flutter-with-firebase/brew_crew/lib/services/auth.dart
@@ -19,7 +19,7 @@ class AuthService {
   // sign in anon
   Future signInAnon() async {
     try {
-      AuthResult  result = await _auth.signInAnonymously();
+      AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
 
       return _userFromFirebaseUser(user);
@@ -32,6 +32,17 @@ class AuthService {
   // sign in with email & password
 
   // register with email & password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // sign out
   Future signOut() async {
