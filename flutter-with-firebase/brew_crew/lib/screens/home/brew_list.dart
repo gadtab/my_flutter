73f5830784d791bd31149b9ff79f5ce06c42f653
Gadi Tabachnik <gadtab@gmail.com>
2020-11-06 10:15:15 +0200

    Listing Brew Data

--- flutter-with-firebase/brew_crew/lib/screens/home/brew_list.dart
+++ flutter-with-firebase/brew_crew/lib/screens/home/brew_list.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,16 +12,10 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    //print(brews.documents);
 
-    brews.forEach((brew) { 
-      print(brew.name);
-      print(brew.strength);
-      print(brew.sugars);
-    });
-
-    return Container(
-      
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) => BrewTile(brew: brews[index]),
     );
   }
 }
