89aef14b70f3edad270eb17d3774da9dc15ddfe6
Gadi Tabachnik <gadtab@gmail.com>
2020-11-06 09:58:08 +0200

    Brew Model

--- flutter-with-firebase/brew_crew/lib/screens/home/home.dart
+++ flutter-with-firebase/brew_crew/lib/screens/home/home.dart
@@ -1,12 +1,17 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/services/database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/screens/home/brew_list.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
       child: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
@@ -23,6 +28,7 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
+        body: BrewList(),
       ),
     );
   }
