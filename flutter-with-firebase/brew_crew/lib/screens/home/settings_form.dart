e067e4b1035da5767a099a29f5eb081f63d80ff1
Gadi Tabachnik <gadtab@gmail.com>
2020-11-06 11:08:05 +0200

    Sliders

--- flutter-with-firebase/brew_crew/lib/screens/home/settings_form.dart
+++ flutter-with-firebase/brew_crew/lib/screens/home/settings_form.dart
@@ -13,7 +13,7 @@ class _SettingsFormState extends State<SettingsForm> {
   // form values
   String _currentName;
   String _currentSugars;
-  String _currentStrength;
+  int _currentStrength;
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +44,15 @@ class _SettingsFormState extends State<SettingsForm> {
             onChanged: (val) => setState(() => _currentSugars = val),
           ),
           // slider
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min: 100,
+            max: 900,
+            divisions: 8,
+            onChanged: (val) => setState(() => _currentStrength = val.round()),
+          ),
           RaisedButton (
             color: Colors.pink[400],
             child: Text(
