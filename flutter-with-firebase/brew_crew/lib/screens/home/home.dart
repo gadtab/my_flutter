478e0e512d40484b606a0621d209c5f908f2f410
Gadi Tabachnik <gadtab@gmail.com>
2020-11-06 10:22:52 +0200

    Bottom Sheets

--- flutter-with-firebase/brew_crew/lib/screens/home/home.dart
+++ flutter-with-firebase/brew_crew/lib/screens/home/home.dart
@@ -10,6 +10,13 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context) => Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+        child: Text('bottom sheet'),
+      ));
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -25,6 +32,11 @@ class Home extends StatelessWidget {
                 await _auth.signOut();
               },
               label: Text('logout'),
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text('settings'),
+              onPressed: () => _showSettingsPanel(),
             ),
           ],
         ),
