8f4f195711d095f8c058b33fce847c585b8eccdf
Gadi Tabachnik <gadtab@gmail.com>
2020-11-09 19:02:45 +0200

    first commit

--- LearnigTheAlphaBet/learn_the_alpha_bet/lib/main.dart
+++ LearnigTheAlphaBet/learn_the_alpha_bet/lib/main.dart
@@ -1,117 +1,306 @@
+import 'package:animated_floatactionbuttons/animated_floatactionbuttons.dart';
 import 'package:flutter/material.dart';
+import 'package:learn_the_alpha_bet/my_painter.dart';
+import 'package:learn_the_alpha_bet/touch_points.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(CanvasPainting());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class CanvasPainting extends StatefulWidget {
+  @override
+  _CanvasPaintingState createState() => _CanvasPaintingState();
+}
+
+class _CanvasPaintingState extends State<CanvasPainting> {
+  List<TouchPoints> points = List();
+
+  double opacity = 1.0;
+  StrokeCap strokeType = StrokeCap.round;
+  double strokeWidth = 3.0;
+  Color selectedColor = Colors.black;
+
+  Future<void> _opacity() async {
+    //Shows AlertDialog
+    return showDialog<void>(
+      context: context,
+
+      //Dismiss alert dialog when set true
+      barrierDismissible: true,
+
+      builder: (BuildContext context) {
+        //Clips its child in a oval shape
+        return ClipOval(
+          child: AlertDialog(
+            //Creates three buttons to pick opacity value.
+            actions: <Widget>[
+              FlatButton(
+                child: Icon(
+                  Icons.opacity,
+                  size: 24,
+                ),
+                onPressed: () {
+                  //most transparent
+                  opacity = 0.1;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.opacity,
+                  size: 40,
+                ),
+                onPressed: () {
+                  opacity = 0.5;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.opacity,
+                  size: 60,
+                ),
+                onPressed: () {
+                  //not transparent at all.
+                  opacity = 1.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _pickStroke() async {
+    //Shows AlertDialog
+    return showDialog<void>(
+      context: context,
+
+      //Dismiss alert dialog when set true
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        //Clips its child in a oval shape
+        return ClipOval(
+          child: AlertDialog(
+            //Creates three buttons to pick stroke value.
+            actions: <Widget>[
+              //Resetting to default stroke value
+              FlatButton(
+                child: Icon(
+                  Icons.clear,
+                ),
+                onPressed: () {
+                  strokeWidth = 3.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.brush,
+                  size: 24,
+                ),
+                onPressed: () {
+                  strokeWidth = 10.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.brush,
+                  size: 40,
+                ),
+                onPressed: () {
+                  strokeWidth = 30.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Icon(
+                  Icons.brush,
+                  size: 60,
+                ),
+                onPressed: () {
+                  strokeWidth = 50.0;
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  List<Widget> fabOption() {
+    return <Widget>[
+      //FAB for choosing stroke
+      FloatingActionButton(
+        heroTag: "paint_stroke",
+        child: Icon(Icons.brush),
+        tooltip: 'Stroke',
+        onPressed: () {
+          //min: 0, max: 50
+          setState(() {
+            _pickStroke();
+          });
+        },
+      ),
+
+      //FAB for choosing opacity
+      FloatingActionButton(
+        heroTag: "paint_opacity",
+        child: Icon(Icons.opacity),
+        tooltip: 'Opacity',
+        onPressed: () {
+          //min:0, max:1
+          setState(() {
+            _opacity();
+          });
+        },
+      ),
+
+      //FAB for resetting screen
+      FloatingActionButton(
+          heroTag: "erase",
+          child: Icon(Icons.clear),
+          tooltip: "Erase",
+          onPressed: () {
+            setState(() {
+              points.clear();
+            });
+          }),
+
+      //FAB for picking red color
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_red",
+        child: colorMenuItem(Colors.red),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.red;
+          });
+        },
+      ),
+
+      //FAB for picking green color
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_green",
+        child: colorMenuItem(Colors.green),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.green;
+          });
+        },
+      ),
+
+      //FAB for picking pink color
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_pink",
+        child: colorMenuItem(Colors.pink),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.pink;
+          });
+        },
+      ),
+
+      //FAB for picking blue color
+      FloatingActionButton(
+        backgroundColor: Colors.white,
+        heroTag: "color_blue",
+        child: colorMenuItem(Colors.blue),
+        tooltip: 'Color',
+        onPressed: () {
+          setState(() {
+            selectedColor = Colors.blue;
+          });
+        },
+      ),
+    ];
+  }
+
+  Widget colorMenuItem(Color color) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedColor = color;
+        });
+      },
+      child: ClipOval(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          height: 36,
+          width: 36,
+          color: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GestureDetector(
+          //Main canvas goes here,
+          onPanStart: (details) {
+            setState(() {
+              RenderBox renderBox = context.findRenderObject();
+              points.add(TouchPoints(
+                  points: renderBox.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeType
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+            });
+          },
+          onPanUpdate: (details) {
+            setState(() {
+              RenderBox renderBox = context.findRenderObject();
+              points.add(TouchPoints(
+                  points: renderBox.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeType
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+            });
+          },
+          onPanEnd: (details) {
+            setState(() {
+              points.add(null);
+            });
+          },
+          child: Stack(
+            children: [
+              Center(
+                child: Text('put an image here'),
+              ),
+              CustomPaint(
+                size: Size.infinite,
+                painter: MyPainter(
+                  pointsList: points,
+                ),
+              ),
+            ],
+          ),
         ),
+        floatingActionButton: AnimatedFloatingActionButton(
+          colorStartAnimation: Colors.blue,
+          colorEndAnimation: Colors.cyan,
+          animatedIconData: AnimatedIcons.menu_close,
+          fabButtons: fabOption(),
+        ), //FAB for the menu items goes here
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ LearnigTheAlphaBet/learn_the_alpha_bet/lib/touch_points.dart
@@ -0,0 +1,8 @@
+//Class to define a point touched at canvas
+import 'package:flutter/material.dart';
+
+class TouchPoints {
+  Paint paint;
+  Offset points;
+  TouchPoints({this.points, this.paint});
+}
